487a17faf3151d2ef49858b6cf5692ae878fe320
Ilya Korol <26299517+we1come32@users.noreply.github.com>
2022-01-01 21:42:08 +0300

    Added pydantic for validation data

--- App/models.py
+++ App/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 from django.utils import timezone
-# from django.contrib.auth.models import User as DjangoUser
+from django.contrib.auth.models import User as DjangoUser
 
 
 class User(models.Model):
-    """
-    Part of the default Django user model
-    """
     name = models.CharField()
-    # user = models.ForeignKey(DjangoUser, on_delete=models.CASCADE, verbose_name='Пользователь', related_name='info')
+    user = models.ForeignKey(DjangoUser, on_delete=models.CASCADE, verbose_name='Пользователь', related_name='info')
 
     def get_correlation(self):
         pass
 
 
 class Value(models.Model):
-    value = models.FloatField(default=0, verbose_name='Значение')
+    name = models.CharField(default='default', max_length=20, verbose_name='Имя значения')
     user = models.ForeignKey(User, on_delete='Пользователь', related_name='values')
-    date = models.DateTimeField(default=timezone.now, verbose_name='Дата')
+    date = models.DateField(default=timezone.now, verbose_name='Дата')
+    value = models.FloatField(default=0, verbose_name='Значение')
--- App/views.py
+++ App/views.py
@@ -1,9 +1,36 @@
-from django.http import JsonResponse
+from typing import List
+from datetime import date
+
+from django.http import JsonResponse, HttpResponse, HttpRequest
+from pydantic import BaseModel, ValidationError
 
 
-def calculate() -> JsonResponse | None:
-    return None
+class Value(BaseModel):
+    date: date
+    value: float
 
 
-def correlation() -> JsonResponse | None:
+class MassValue(BaseModel):
+    x_data_type: str
+    y_data_type: str
+    x: List[Value]
+    y: List[Value]
+
+
+class Data(BaseModel):
+    user_id: int
+    data: MassValue
+
+
+def calculate(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        try:
+            a = Data(**request.POST)
+        except ValidationError:
+            return HttpResponse("Unknown data format", status=400)
+        return HttpResponse('ok')
+    return HttpResponse('This method use POST method', status=400)
+
+
+def correlation(request: HttpRequest) -> JsonResponse | None:
     return None
